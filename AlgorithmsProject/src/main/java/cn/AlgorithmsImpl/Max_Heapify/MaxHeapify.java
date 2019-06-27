b39c4c78efa93d048f1bd4dede69f7aa5a72f532
帅哥 <10@qq.com>
2019-06-27 16:53:33 +0800

    重构了最大堆的实现

--- AlgorithmsProject/src/main/java/cn/AlgorithmsImpl/Max_Heapify/MaxHeapify.java
+++ AlgorithmsProject/src/main/java/cn/AlgorithmsImpl/Max_Heapify/MaxHeapify.java
@@ -5,50 +5,69 @@ package cn.AlgorithmsImpl.Max_Heapify;
  * 注意，最大堆的性质只是父结点大于子节点而已！！在数组中不一定是从大到小的排列的！！！！
  * 如果想要从大到小或从小到大排序，只能不断的抽取首个元素，直到数组消失
  */
-public class MaxHeapify {
-    private static int[] space;
-    private static int length;
+public class MaxHeapify<Key extends Comparable<Key>> {
+    private Key[] data;
+    private int N = 0;
 
-    private static int left(int i) {
-        return 2 * (i + 1) - 1;
+    public MaxHeapify(int Max) {
+        data = (Key[]) new Comparable[Max + 1];
     }
 
-    private static int right(int i) {
-        return 2 * (i + 1);
-    }
-
-    private static void checkMaxHeapify(int i) {
-        int max = i;
-        int l = left(i);
-        int r = right(i);
-        if (l < length && space[l] > space[i]) max = l;
-        if (r < length && space[r] > space[max]) max = r;
-        if (max != i) {
-            int temp = space[max];
-            space[max] = space[i];
-            space[i] = temp;
-            checkMaxHeapify(max);
+    public void showData() {
+        for (int i = 0; i < N; i++) {
+            System.out.print(data[i + 1] + " ");
         }
     }
 
-    private static void buildMaxHeap(int[] array, int arrayLength) {
-        space = array;
-        length = arrayLength;
-        for (int i = (int) Math.floor(length / 2); i >= 0; i--) {
-            checkMaxHeapify(i);
+    //比较
+    private boolean less(int i, int j) {
+        return data[i].compareTo(data[j]) < 0;
+    }
+
+    //交换
+    private void exch(int i, int j) {
+        Key temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+
+    //上浮
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
         }
     }
 
-    public static void heapSort(int[] maxHeapifyArray) {
-        for (int i = maxHeapifyArray.length; i > 0; i--) {
-            buildMaxHeap(maxHeapifyArray, i);
-            int temp = maxHeapifyArray[0];
-            maxHeapifyArray[0] = maxHeapifyArray[i - 1];
-            maxHeapifyArray[i - 1] = temp;
+    //下沉
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) j++;
+            if (less(j, k)) break;
+            exch(j, k);
+            k = j;
         }
     }
 
-    private int parent(int i) {
-        return (int) Math.floor(i / 2);
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        data[++N] = v;
+        swim(N);
+    }
+
+    public Key deleteMax() {
+        Key result = data[1];
+        data[1] = data[N--];
+        data[N + 1] = null;
+        sink(1);
+        return result;
     }
 }
