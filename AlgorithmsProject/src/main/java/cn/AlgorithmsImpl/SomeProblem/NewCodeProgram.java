05f32a886486c893440b278242377dca84a84da8
帅哥 <10@qq.com>
2019-06-28 11:42:47 +0800

    添加了牛客网算法题部分实现

--- AlgorithmsProject/src/main/java/cn/AlgorithmsImpl/SomeProblem/NewCodeProgram.java
+++ AlgorithmsProject/src/main/java/cn/AlgorithmsImpl/SomeProblem/NewCodeProgram.java
@@ -34,8 +34,77 @@ public class NewCodeProgram {
 //        }
 //        return result;
 //    }
-
+    //实现具有Min方法的栈
+//    Stack stack1 = new Stack();
+//    Stack stack2 = new Stack();
+//
+//    public void push(int node) {
+//        stack1.push(node);
+//        if (stack2.isEmpty()) {
+//            stack2.push(node);
+//        } else {
+//            if ((int) stack2.peek() <= node) {
+//                stack2.push((int) stack2.peek());
+//            } else stack2.push(node);
+//        }
+//    }
+//
+//    public void pop() {
+//        stack1.pop();
+//        stack2.pop();
+//    }
+//
+//    public int min() {
+//        int min = (int) stack2.peek();
+//        return min;
+//    }
+    //反转链表
+//    public static class ListNode {
+//        int val;
+//        ListNode next = null;
+//
+//        ListNode(int val) {
+//            this.val = val;
+//        }
+//    }
+//    ListNode result;
+//    public ListNode ReverseList(ListNode head){
+//        if(head.next == null){
+//            result = head;
+//            return head;
+//        }
+//        ReverseList(head.next);
+//        head.next.next = head;
+//        head.next = null;
+//        return result;
+//    }
+    //斐波那契数列
+//    public int Fibonacci(int n){
+//        int result;
+//        if(n == 0)return 0;
+//        if(n == 1)return 1;
+//        if(n == 2)return 1;
+//        result = Fibonacci(n - 1) + Fibonacci(n - 2);
+//        return result;
+//    }
+    //跳台阶
+//    public int JumpFloor(int target){
+//        if(target == 1)return 1;
+//        if(target == 2)return 2;
+//        return JumpFloor(target - 1) + JumpFloor(target - 2);
+//    }
+    //跳台阶II
+//    public int JumpFloorII(int target){
+//        if(target == 0)return 0;
+//        if(target == 1)return 1;
+//        if(target == 2)return 2;
+//        int result = 0;
+//        while(target != 0){
+//            result += JumpFloorII(--target);
+//        }
+//        return result + 1;
+//    }
     public static void main(String[] args) {
-
+        NewCodeProgram newCodeProgram = new NewCodeProgram();
     }
 }
